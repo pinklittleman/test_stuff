80531d6533eaa8e427512e0a043eecace6d8517a
pinklittleman <theominer3000@gmail.com>
2023-06-28 22:05:02 +0100

    new stuff

--- pixels/index.js
+++ pixels/index.js
@@ -1,7 +1,145 @@
-const canvas = document.querySelector('canvas')
-const ctx = canvas.getContext('2d')
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
 
 canvas.width = innerWidth
 canvas.height = innerHeight
 
-let balls = []
+const BALLZ = [];
+
+let LEFT, UP, RIGHT, DOWN;
+let friction = 0.1;
+
+//a class Vector with basic vector operations
+class Vector{
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+
+    add(v){
+        return new Vector(this.x+v.x, this.y+v.y);
+    }
+
+    subtr(v){
+        return new Vector(this.x-v.x, this.y-v.y);
+    }
+
+    mag(){
+        return Math.sqrt(this.x**2 + this.y**2)
+    }
+
+    mult(n){
+        return new Vector(this.x*n, this.y*n);
+    }
+
+    drawVec(start_x, start_y, n, color){
+        ctx.beginPath();
+        ctx.moveTo(start_x, start_y);
+        ctx.lineTo(start_x + this.x * n, start_y + this.y * n);
+        ctx.strokeStyle = color;
+        ctx.stroke();
+        ctx.closePath();
+    }
+}
+
+class Ball{
+    constructor(x, y, r){
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.vel = new Vector(0,0);
+        this.acc = new Vector(0,0);
+        this.acceleration = 1;
+        this.player = false;
+        BALLZ.push(this);
+    }
+
+    drawBall(){
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.r, 0, 2*Math.PI);
+        ctx.strokeStyle = "black";
+        ctx.stroke();
+        ctx.fillStyle = "red";
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    display(){
+        this.vel.drawVec(this.x, this.y, 10, "green");
+        this.acc.drawVec(this.x, this.y, 100, "blue");
+    }
+}
+
+function keyControl(b){
+    canvas.addEventListener('keydown', function(e){
+        if(e.key === "a"){
+            LEFT = true;
+        }
+        if(e.key === "w"){
+            UP = true;
+        }
+        if(e.key === "d"){
+            RIGHT = true;
+        }
+        if(e.key === "s"){
+            DOWN = true;
+        }
+    });
+    
+    canvas.addEventListener('keyup', function(e){
+        if(e.key === "a"){
+            LEFT = false;
+        }
+        if(e.key === "w"){
+            UP = false;
+        }
+        if(e.key === "d"){
+            RIGHT = false;
+        }
+        if(e.key === "s"){
+            DOWN = false;
+        }
+    });
+    
+    if(LEFT){
+        b.acc.x = -b.acceleration;
+    }
+    if(UP){
+        b.acc.y = -b.acceleration;
+    }
+    if(RIGHT){
+        b.acc.x = b.acceleration;
+    }
+    if(DOWN){
+        b.acc.y = b.acceleration;
+    }
+    if(!LEFT && !RIGHT){
+        b.acc.x = 0;
+    }
+    if(!UP && !DOWN){
+        b.acc.y = 0;
+    }
+    
+    //acceleration vector gets added to the velocity vector
+    b.vel = b.vel.add(b.acc);
+    b.vel = b.vel.mult(1-friction);
+    b.x += b.vel.x;
+    b.y += b.vel.y;
+}
+
+function mainLoop(timestamp) {
+    ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+    BALLZ.forEach((b) => {
+        b.drawBall();
+        if (b.player){
+            keyControl(b);
+        }
+        b.display();
+    })
+    requestAnimationFrame(mainLoop);
+}
+
+let Ball1 = new Ball(200, 200, 30);
+Ball1.player = true;
+
+requestAnimationFrame(mainLoop);
